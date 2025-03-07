a870c4b96503ab98252710148ba3e3223ce2ad16
kiran-kurian <kirankurian02@gmail.com>
2025-03-07 23:18:01 +0530

    updates

--- pages/login.vue
+++ pages/login.vue
@@ -5,14 +5,15 @@
         <div class="window">
         <div class="inactive-title-bar">
 
-            <h1 class="title">Login Now</h1>
+            <h1 class="title">
+                <input type="text" v-model="a"></input></h1>
 
         </div>
         </div>
         <label for="text_email">Email</label><br>
-        <input id="text_email" type="email" placeholder="panic@thedis.co"/><br>
+        <input id="text_email" type="email"/><br>
         <label for="text_pwd">Password</label><br>
-        <input id="text_pwd" type="password" placeholder="password"/><br><br>
+        <input id="text_pwd" type="password"/><br><br>
         <button class="btn">Login</button>
     </center>
 </form>
@@ -21,6 +22,10 @@
 
 <style scoped>
     form {
-        margin-top: 20%;
+        margin-top: 15%;
     }
 </style>
+
+<script lang="ts" setup>
+    const a = "Login Now";
+</script>
