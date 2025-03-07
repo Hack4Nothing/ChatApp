d966a6384be7269742e9fd8e0e338a5c8c5950b3
MatthewsM2 <matthew1773@gmail.com>
2025-03-08 00:37:25 +0530

    AppFooter added and default layout flex

---
+++ components/AppFooter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="standard-dialog center scale-down" style="width:30rem;">
+  <h1 class="dialog-text"><input type="text" class="normal-text" v-model=versionAndName></h1>
+  <p class="dialog-text"> <input type="text" class="normal-text" v-model=copyrightYear>  </p>
+</div>
+</template>
+
+<style scoped>
+@import './assets/css/chatapp-css.css';
+div{
+    margin: auto;
+    width: 95vw !important;
+}
+input {
+    /* text-align: left; */
+    width: 100% !important;
+}
+</style>
+
+<script setup>
+const versionAndName = "ChatApp, Version 1.0 (jan 01 2025)"
+const copyrightYear = "© 2025 Apple Computer";
+
+
+
+ </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,18 @@
 <template>
-    <AppHeader/>
-    <slot/>
-    <AppFooter/>
+    <div id="app">
+        <AppHeader />
+        <slot />
+        <AppFooter />
+    </div>
 </template>
 
 <style scoped>
-    @import 'https://unpkg.com/@sakun/system.css';
+@import 'https://unpkg.com/@sakun/system.css';
+
+#app {
+    display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  justify-content: space-between;
+}
 </style>
